86431d858f3cbce2107df447133fc6945a5338d1
r3nniw <r3nniw@Krikchais-MacBook-Pro.local>
2017-09-26 00:22:37 +0700

    ❌ filtered out querystring urls + depth level checking + ignore output.txt

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// TODO: parent link logging 
+// TODO: parent link logging
 //       เพื่อดูว่าจำเป็นต้องลบ link ที่เป็น qs หรือไม่
-// TODO: อ่านวิธีการเขียน crawler ที่ดีจาก slides
+// TODO: จัดการกับ URI.origin/robots.txt
+// TODO: ทำ counter ไว้นับจำนวน request
 
 // ##### dependencies #####
 
 const R = require('ramda')
-const {  } = require('ramda-fantasy')
+// const {  } = require('ramda-fantasy')
 const Crawler = require('crawler')
 const Seenreq = require('seenreq')
 const Urlparse = require('url-parse')
@@ -14,63 +15,77 @@ const Urlparse = require('url-parse')
 
 const linkDb = new Seenreq()
 const crawler = new Crawler()
-const seedUrl = 'https://www.cpe.ku.ac.th'
+const seedUrl = 'http://www.ku.ac.th/web2012/'
 
-// ##### pointfree functions #####
+// ##### functions #####
 
-const isKuSite = R.pipe(
-  R.unary(Urlparse), // prevent side effects
-  R.allPass([
-    R.propSatisfies(R.complement(R.isEmpty), 'slashes'),
-    R.propSatisfies(R.test(/^http(s)?:/), 'protocol'),
-    R.propSatisfies(R.test(/ku\.ac\.th$/), 'hostname')
-  ])
-)
+// TODO: compose logger หลายๆตัวรวมเป็นกลุ่มเดียว
+const tap2Log = (tag, what2Log = R.identity) => R.tap(x => (
+  console.log('##### @', tag),
+  console.log(what2Log(x))
+))
 
-const isHtml = R.pathSatisfies(
+const isHtmlResponse = R.pathSatisfies(
   R.test(/text\/html/),
   ['headers', 'content-type']
 )
 
-const nextLevelUrls = R.ifElse(
-  isHtml,
-  R.pipe(R.prop('$'), filteredUrls),
-  R.always([])
+const isKuSite = R.allPass([
+  R.propSatisfies(R.complement(R.isEmpty), 'slashes'),
+  R.propSatisfies(R.test(/^http(s)?:/), 'protocol'),
+  R.propSatisfies(R.test(/ku\.ac\.th$/), 'hostname')
+])
+
+const sanitizeUrl = R.pipe(
+  R.unary(Urlparse),
+  x => (x.set('query', ''), x.set('hash', ''), x)
 )
 
-// ##### functions (expression style) #####
-
-// ใช้ R.allPass ไม่ได้ไม่รู้เพราะว่าอะไร?
-const passQueueingConditions = url =>
-  isKuSite(url) && !linkDb.exists(url)
+const passQueueingConditions = R.allPass([
+  isKuSite,
+  x => !linkDb.exists(x.href) // !! WARNING: SIDE CAUSES/EFFECTS !!
+])
 
 // IIFE with empty array initialized
 const filteredUrls = $ => (arr => {
   $('a').each(function() {
-    const candidateUrl = $(this).attr('href')
+    const candidateUrl = sanitizeUrl($(this).attr('href'))
+
     if(passQueueingConditions(candidateUrl)) {
-      arr.push(candidateUrl)
+      arr.push(candidateUrl.href)
     }
   })
 
   return arr
 })([])
 
+const nextUrls = R.ifElse(
+  isHtmlResponse,
+  R.pipe(R.prop('$'), filteredUrls),
+  R.always([])
+)
+
 // ##### application #####
 
-// initialize with seed url
-crawler.queue({ uri: seedUrl, callback })
-
 function callback(err, res, done) {
-  const nextLevelReqs = R.pipe(
-    R.tap(x => console.log('##### @', x.request.uri.href)),
-    R.tap(x => console.log('##### @ content-type', x.headers['content-type'])),
-    nextLevelUrls,
-    R.tap(x => ( console.log('##### @ nextLevelUrls'), console.log(x) )),
-    R.map( uri => ({ uri, callback }) ),
-    // R.tap(x => ( console.log('##### @ nextLevelReqs'), console.log(x) )),
+  const nextRequests = R.pipe(
+    tap2Log('href', R.path([ 'request', 'uri', 'href' ])),
+    tap2Log('depth level', R.path([ 'options', 'depth' ])),
+    nextUrls,
+    tap2Log('to be put in queue'),
+    R.map(uri => ({
+      uri,
+      callback,
+      depth: res.options.depth + 1
+    }))
   )(res)
 
-  crawler.queue(nextLevelReqs) // non-blocking operation
+  // !! WARNING: SIDE CAUSES/EFFECTS !!
+  // non-blocking operation
+  crawler.queue(nextRequests)
   done()
 }
+
+// !! WARNING: SIDE CAUSES/EFFECTS !!
+// initialize with seed url
+crawler.queue({ uri: seedUrl, callback, depth: 0 })
