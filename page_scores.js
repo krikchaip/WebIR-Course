040fa683142df3d0911f9aa4537925e6789d0206
r3nniw <r3nniw@Krikchais-MacBook-Pro.local>
2017-11-24 22:46:57 +0700

    completed page_score

--- page_scores.js
+++ page_scores.js
@@ -3,9 +3,10 @@ const { Either } = require('ramda-fantasy')
 const Future = require('fluture')
 const { inspect } = require('util')
 const { BigNumber } = require('bignumber.js')
+const { Matrix } = require('vectorious')
 
 const arrayLog = arr =>
-  console.log(inspect(arr, { breakLength: 1000000, maxArrayLength: null }))
+  console.log(inspect(arr, { breakLength: 0, maxArrayLength: null }))
 
 const fromNullable = _.ifElse(_.isNil, Either.Left, Either.Right)
 
@@ -53,15 +54,36 @@ const randomSurferWeight = alpha => matrix =>
     row.map(col => (
       a = new BigNumber(alpha),
       ia = a.negated().plus(1),
-      newP = a.mul(col)
-      .plus(ia.mul(divide(1, matrix.length)))
-      .toFixed(15),
+      newP =
+        a.mul(col)
+        .plus(ia.mul(divide(1, matrix.length)))
+        .toFixed(15),
       Number(newP) // result
     )))
 
+/* mimic from pagerank algorithm(lecture 9, 32) */
+const pageRank = iteration => matrix => {
+  let A, R, R0, i = 0
+  A = new Matrix(_.transpose(matrix))
+  R = new Matrix(_.transpose([stochastic(matrix.length)]))
+
+  do { R0 = R; R = A.multiply(R0); }
+  while(++i < iteration)
+
+  return R.toArray()
+}
+
+const stochastic = n =>
+  _.repeat(divide(1, n), n)
+
+const discreteNorm = matrix =>
+  _.flatten(matrix).reduce((acc, x) =>
+    Math.abs(acc) + Math.abs(x), 0)
+
 readStdinSync
 .chain(webgraph)
-// .map(_.take(100))
 .map(rowStochastic)
 .map(randomSurferWeight(0.85))
+.map(pageRank(100))
+// .map(_.pipe(_.flatten, _.reduce(_.add, 0)))
 .fork(console.error, arrayLog)
