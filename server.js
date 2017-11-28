2346648bde8e91b8c5b188f33d378a08410d6a47
r3nniw <r3nniw@Krikchais-MacBook-Pro.local>
2017-11-29 00:23:47 +0700

    finished simple API

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const Path = require('path')
+const Express = require('express')
 const BodyParser = require('body-parser')
-const server = require('express')()
+const search = require('./search/searchfiles')
+
+const server = Express()
 
 server.use(BodyParser.urlencoded({ extended: false }))
-server.post('/', (req, res) => res.send(req.body))
+server.use(Express.static(Path.join(__dirname, 'frontend')))
+server.get('/', (req, res) => res.sendFile('index.html'))
+server.get('/search', (req, res) => (
+  result =
+    search(req.query.term, req.query.weight)
+    .either(() => {}, r => r),
+  res.json(result)
+))
 
-server.listen(3000, () => console.log(`server listening at port 3000`))
+server.listen(3000, () =>
+  console.log(`Success, start browsing at http://localhost:3000`))
